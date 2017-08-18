3ddd4695a7ddf805076f1e88100b5738286ccf3f
James Thomas <james9074@gmail.com>
2017-08-18 19:49:17 -0400

    Cleaning up build

--- ui/config/env.js
+++ ui/config/env.js
@@ -58,7 +58,6 @@ process.env.NODE_PATH = (process.env.NODE_PATH || '')
 const REACT_APP = /^REACT_APP_/i;
 
 function getClientEnvironment(publicUrl) {
-  console.log(process.env)
   const raw = Object.keys(process.env)
     .filter(key => REACT_APP.test(key))
     .reduce(
