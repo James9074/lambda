7c95c2c500d3b2810d532278ceeb7cce86f9f4b5
James Thomas <james9074@gmail.com>
2017-08-18 22:30:43 -0400

    Gutting service worker for better api support

--- ui/src/index.js
+++ ui/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter } from 'react-router-dom'
 import { ApolloClient, ApolloProvider, createNetworkInterface } from 'react-apollo';
 
@@ -24,4 +23,3 @@ ReactDOM.render((
     </BrowserRouter>
   </ApolloProvider>        
 ), document.getElementById('root'))
-registerServiceWorker();
